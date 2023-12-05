28293c775ef99f1efa8b858a515d6bc0e6797a1a
gjancec <gjancec@gmail.com>
2023-12-05 04:41:05 +0100

    Second

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,10 +1,52 @@
+import { FaInstagram, FaLinkedin ,FaFacebook, FaTwitter} from 'react-icons/fa'
+
 
 const Footer = () => {
     return (
       <footer className="h-64 bg-red pt-10">
         <div className="w-10/12 mx-auto">
-          social icons
-          <div className="md:flex justify-center md:justify-between text-center ">
+        <div className='flex justify-center md:justify-start my-4 gap-7 text-gray-600'>
+            <a 
+             className='hover:opacity-50 transition duration-500' 
+             size={20}
+             href="https://www.instagram.com/"
+             target="_blank"
+             rel="noreferrer"
+            >
+            <FaInstagram size={20} />
+            </a>
+            <a 
+              className='hover:opacity-50 transition duration-500' 
+              size={20}
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+              >
+                <FaFacebook size={20}/>
+             </a>
+            
+            
+            <a 
+              className='hover:opacity-50 transition duration-500' 
+              size={20}
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noreferrer"
+              >
+                <FaLinkedin size={20}/>
+             </a>
+             <a 
+              className='hover:opacity-50 transition duration-500' 
+              size={20}
+              href="https://www.twitter.com/"
+              target="_blank"
+              rel="noreferrer"
+              >
+                <FaTwitter  size={20}/>
+             </a>
+            
+          </div>
+                   <div className="md:flex justify-center md:justify-between text-center ">
             <p className="font-playfair font-semibold text-2xl text-yellow">
               FLOWER GALERY
             </p>
--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -152,40 +152,4 @@ const Landing = ({setSelectedPage}) => {
    
   
   
-  {/*  <img className='w-full h-screen object-cover object-right' src= {Background} />
-      
-      
-      <div className='w-full h-screen absolute top-0 left-0 bg-white/5 '> 
-      <div className='max-w-[850px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center' >
-          <h1 className='sm:text-6xl text-4xl font-bold text-black'> Flower Gallery</h1>
-          <h2 className='flex sm:text-4xl text 2xl pt-4 text-black'> Plants in your home  
-            <TypeAnimation
-              sequence={[
-                // Same substring at the start will only be typed out once, initially
-                'Revitalize your energy',
-                2000, // wait 1s before replacing "Mice" with "Hamsters"
-                'Enchance your mood',
-                2000,
-                'Reduce stress',
-                2000,
-              
-              ]}
-              wrapper="span"
-              speed={50}
-              style={{ fontSize: '1em', paddingLeft: '5px'  }}
-              repeat={Infinity}
-            />
-          </h2>
-          <div className='flex justify-between pt-6 max-w-[200px] w-full text-gray-300'>
-            <FaInstagram className='cursor-pointer' size={20}/>
-            <FaFacebook className='cursor-pointer' size={20}/>
-            <FaLinkedin className='cursor-pointer' size={20}/>
-            <FaTwitter className='cursor-pointer' size={20}/>
-          </div>
-          </div>
-      
-    
-    
-   
-  
-*/}
+ 
