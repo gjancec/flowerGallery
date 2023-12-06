f584524fbb2cb2b6641835aeb3901d314f0b8395
gjancec <gjancec@gmail.com>
2023-12-06 03:21:21 +0100

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/flowerShop/",
+  
   plugins: [react()],
+  base:"/flowerShop/",
 })
